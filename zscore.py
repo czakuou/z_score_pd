784f54e2795464ef33bc8f1f420c3b7bfe046cbc
czakuou <emil.krzakuou@gmail.com>
2021-03-14 22:34:33 +0100

    split training data

--- zscore.py
+++ zscore.py
@@ -1,7 +1,11 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 import numpy as np
 from pandas.plotting import scatter_matrix
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
 
 # import data
 df = pd.read_csv("countryriskdata.csv", sep=",")
@@ -14,6 +18,10 @@ df[num_cols] = scaler.fit_transform(df[num_cols])
 
 # plot and calculate correlation
 corr_matrix = df[num_cols].corr()
-print(corr_matrix)
 scatter_matrix(df[num_cols], figsize=(12, 8))
-plt.show()
+# plt.show()
+
+# regression model. predict corruption based od legal par
+data_X, data_y = df['Legal'], df['Corruption']
+X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size=0.33, random_state=42)
+print(y_train)
